8548ff3634a4e1f758e992d59f791d76f3bcdfb2
Blannon <63664876+Blannon@users.noreply.github.com>
2023-08-18 02:49:49 +0300

    QR code generator in java

---
+++ app/src/main/java/com/blannoncodes/qrgeneratorinjava/MainActivity.java
@@ -0,0 +1,52 @@
+package com.blannoncodes.qrgeneratorinjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.blannoncodes.qrgeneratorinjava.databinding.ActivityMainBinding;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.encoder.QRCode;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.edt_entertext);
+        Button button = findViewById(R.id.generatebtn);
+        ImageView imageView = findViewById(R.id.qrcode);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(editText.getText().toString(), BarcodeFormat.QR_CODE, 300,300);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+                    imageView.setImageBitmap(bitmap);
+                }catch (WriterException e){
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+    }
+}
